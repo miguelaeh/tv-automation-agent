b197a98b819a86b27697a221ed70e076263f1ab1
Miguel A. Cabrera Minagorri <devgorri@gmail.com>
2024-05-11 14:42:51 +0200

    Add logic and diagram
    Signed-off-by: Miguel A. Cabrera Minagorri <devgorri@gmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# agent-test
+# Pipeless Agent for TV automation
+
+This agent turns off the TV when nobody is watching it.
+
+This agent works along with the Pipeless Agent Home Assistant Integration.
+
+See the complete tutorial at: LINK VIDEO HERE
+
+
+## Logic of the agent
+
+```mermaid
+graph TD;
+    A[start] --> B{Person};
+    B --> |No| C{TV};
+    B --> |Yes| A;
+    C --> |ON| D{IdleTime};
+    C --> |OFF| A;
+    D --> |Passed| E[Call Webhook];
+    D --> |Not passed| A;
+    E --> A;
+```
--- agent.py
+++ agent.py
@@ -1,6 +1,62 @@
 from pipeless_agents_sdk.cloud import data_stream
+import time
+import requests
+import os
 
-print(f"Ready! Add a pipeline to start receiving data!")
+hass_webhook_url = os.getenv("HASS_WEBHOOK_URL")
+seconds_without_person = os.getenv("SECONDS_WITHOUT_PERSON")
+person_conf = os.getenv("PERSON_CONFIDENCE_THR")
+tv_conf = os.getenv("TV_CONFIDENCE_THR")
+
+if not hass_webhook_url:
+    raise "Missing HAAS_WEBHOOK_URL env var. Please add it to the agent configuration"
+if not seconds_without_person:
+    raise "Missing SECONDS_WITHOUT_PERSON env var. Please add it to the agent configuration"
+if not person_conf:
+    person_conf = 0.65
+    print(f"PERSON_CONFIDENCE_THR env var not specified. Defaulting to {person_conf}. You can adjust the confidence by providing the env var in the agent configuration")
+if not tv_conf:
+    tv_conf = 0.7
+    print(f"TV_CONFIDENCE_THR env var not specified. Defaulting to {tv_conf}. You can adjust the confidence by providing the env var in the agent configuration")
+
+print(f"Agent running! Add a pipeline using Home Assistant as video source to start automating your home with vision!")
+
+tv_on = True # Whether the TV is on (true) or off (false)
+prev_person = False # Whether there is a person
+person_leaves_at = 0 # time at which the person leaves.
+
+def person_present(data):
+    for item in data:
+        class_name = item.get('class_name', None)
+        score = item.get('score', None)
+        if class_name == 'person' and score > person_conf:
+            return True
+    return False
+
+def is_tv_on(data):
+    for item in data:
+        class_name = item.get('class_name', None)
+        score = item.get('score', None)
+        if class_name == 'TV on' and score > tv_conf:
+            return True
+    return False
+
+def turn_off_tv():
+    # If there is no person in 30 seconds, Send webhook to turn off TV.
+    response = requests.get(hass_webhook_url)
+    if response.status_code == 200:
+        print(f"TV turned OFF since there no one watching in more than {seconds_without_person} seconds")
+    else:
+        print("ERROR: turning off TV. Webhook request returned: {response.status_code} status")
+
 for payload in data_stream:
-    print(f"New data received: {payload.value}")
-
+    current_person = person_present(payload)
+    if not current_person and prev_person:
+        person_leaves_at = time.time()
+        prev_person = False
+    elif not current_person and not prev_person and is_tv_on(payload):
+        elapsed = time.time() - person_leaves_at
+        if elapsed > seconds_without_person:
+            turn_off_tv()
+    else:
+        prev_person = True
