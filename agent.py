6de59e9293b1b99b4a7a614701f31f7689592a98
Miguel A. Cabrera Minagorri <devgorri@gmail.com>
2024-05-13 12:29:51 +0200

    Parse json payload to dict
    Signed-off-by: Miguel A. Cabrera Minagorri <devgorri@gmail.com>

--- agent.py
+++ agent.py
@@ -2,6 +2,7 @@ from pipeless_agents_sdk.cloud import data_stream
 import time
 import requests
 import os
+import json
 
 hass_webhook_url = os.getenv("HASS_WEBHOOK_URL")
 seconds_without_person = os.getenv("SECONDS_WITHOUT_PERSON")
@@ -14,12 +15,12 @@ if not seconds_without_person:
     raise "Missing SECONDS_WITHOUT_PERSON env var. Please add it to the agent configuration"
 if not person_conf:
     person_conf = 0.65
-    print(f"PERSON_CONFIDENCE_THR env var not specified. Defaulting to {person_conf}. You can adjust the confidence by providing the env var in the agent configuration")
+    print(f"Warning: PERSON_CONFIDENCE_THR env var not specified. Defaulting to {person_conf}. You can adjust the confidence by providing the env var in the agent configuration\n")
 if not tv_conf:
     tv_conf = 0.7
-    print(f"TV_CONFIDENCE_THR env var not specified. Defaulting to {tv_conf}. You can adjust the confidence by providing the env var in the agent configuration")
+    print(f"Warning: TV_CONFIDENCE_THR env var not specified. Defaulting to {tv_conf}. You can adjust the confidence by providing the env var in the agent configuration\n")
 
-print(f"Agent running! Add a pipeline using Home Assistant as video source to start automating your home with vision!")
+print(f"READY! Add a pipeline using Home Assistant as video source to start automating your home with vision!")
 
 tv_on = True # Whether the TV is on (true) or off (false)
 prev_person = False # Whether there is a person
@@ -50,6 +51,7 @@ def turn_off_tv():
         print("ERROR: turning off TV. Webhook request returned: {response.status_code} status")
 
 for payload in data_stream:
+    payload = json.loads(payload)
     current_person = person_present(payload)
     if not current_person and prev_person:
         person_leaves_at = time.time()
