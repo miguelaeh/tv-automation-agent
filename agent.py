ece076b42ae1dccef0ce4709f9d08e4ba0d433fb
Miguel A. Cabrera Minagorri <devgorri@gmail.com>
2024-05-13 12:43:14 +0200

    Parse float values from string
    Signed-off-by: Miguel A. Cabrera Minagorri <devgorri@gmail.com>

--- agent.py
+++ agent.py
@@ -4,18 +4,24 @@ import requests
 import os
 
 hass_webhook_url = os.getenv("HASS_WEBHOOK_URL")
-seconds_without_person = os.getenv("SECONDS_WITHOUT_PERSON")
-person_conf = os.getenv("PERSON_CONFIDENCE_THR")
-tv_conf = os.getenv("TV_CONFIDENCE_THR")
+seconds_without_person_str = os.getenv("SECONDS_WITHOUT_PERSON")
+person_conf_str = os.getenv("PERSON_CONFIDENCE_THR")
+tv_conf_str = os.getenv("TV_CONFIDENCE_THR")
 
 if not hass_webhook_url:
     raise "Missing HASS_WEBHOOK_URL env var. Please add it to the agent configuration"
-if not seconds_without_person:
+if seconds_without_person_str:
+    seconds_without_person = float(seconds_without_person_str)
+else:
     raise "Missing SECONDS_WITHOUT_PERSON env var. Please add it to the agent configuration"
-if not person_conf:
+if person_conf_str:
+    person_conf = float(person_conf_str)
+else:
     person_conf = 0.65
     print(f"Warning: PERSON_CONFIDENCE_THR env var not specified. Defaulting to {person_conf}. You can adjust the confidence by providing the env var in the agent configuration\n")
-if not tv_conf:
+if tv_conf_str:
+    tv_conf = float(tv_conf_str)
+else:
     tv_conf = 0.7
     print(f"Warning: TV_CONFIDENCE_THR env var not specified. Defaulting to {tv_conf}. You can adjust the confidence by providing the env var in the agent configuration\n")
 
